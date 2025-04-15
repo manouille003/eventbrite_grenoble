56c26cfea7060513b64b6c225245b18332613ba6
manouille003 <lemoinejulia88@gmail.com>
2025-04-15 22:15:26 +0200

    this is all I can do for today

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     validates :email, presence: true, uniqueness: true
-    validates :first_name, presence: true
-    validates :last_name, presence: true
+    validates :password, presence: true, length: { minimum: 6 }
+    validates :password_confirmation, presence: true, length: { minimum: 6 }
 
     after_create :welcome_send
 
